6e0730837bb45df049e7f4b4ae632e1bacfdb53f
Vadym Kharchuk <vadymkharchuk.wk@gmail.com>
2023-06-14 15:19:22 +0300

    [refactor] dfu_usb: Rename library enty point file
    Signed-off-by: Vadym Kharchuk <vadymkharchuk.wk@gmail.com>

---
+++ src/dfu_usb_lib/dfu_usb.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "dfu_usb.h"
+
+int dfu_usb_library_init(void)
+{
+        return 0;
+}
+
+void dfu_usb_library_deinit(void)
+{
+}
---
+++ src/dfu_usb_lib/dfu_usb.h
@@ -0,0 +1,7 @@
+#ifndef __DFU_USB_H__
+#define __DFU_USB_H__
+
+int dfu_usb_library_init(void);
+void dfu_usb_library_deinit(void);
+
+#endif /* __DFU_USB_H__ */
