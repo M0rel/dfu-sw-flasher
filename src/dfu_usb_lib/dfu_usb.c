43e2cf4b5ff23cb2bb1f5079b9c05af40b05803e
Vadym Kharchuk <vadymkharchuk.wk@gmail.com>
2023-06-15 18:15:56 +0300

    dfu_usb: Add library initialization
    Signed-off-by: Vadym Kharchuk <vadymkharchuk.wk@gmail.com>

--- src/dfu_usb_lib/dfu_usb.c
+++ src/dfu_usb_lib/dfu_usb.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 
 #include "dfu_usb.h"
+#include "dfu_usb_error.h"
 
-int dfu_usb_library_init(void)
+dfu_usb_dev_st_t dfu_usb_dev = {0};
+
+dfu_usb_error_t dfu_usb_library_init(void)
 {
-        return 0;
+        int err = LIBUSB_SUCCESS;
+
+        err = libusb_init(&dfu_usb_dev.ctx);
+        if (LIBUSB_SUCCESS != err) {
+                log_err("Failed to initialize USB library. Error: %s\n",
+                        libusb_strerror(err));
+
+                return DFU_USB_INITIALIZATION_FAILED;
+        }
+
+        log_dbg("USB library initialized\n");
+
+        return DFU_USB_SUCCESS;
 }
 
 void dfu_usb_library_deinit(void)
 {
+        libusb_exit(dfu_usb_dev.ctx);
+
+        log_dbg("USB library de-initialized\n");
 }
--- src/dfu_usb_lib/dfu_usb.h
+++ src/dfu_usb_lib/dfu_usb.h
@@ -1,7 +1,16 @@
 #ifndef __DFU_USB_H__
 #define __DFU_USB_H__
 
-int dfu_usb_library_init(void);
+#include <libusb-1.0/libusb.h>
+
+#include "logger.h"
+#include "dfu_usb_error.h"
+
+typedef struct dfu_usb_dev {
+        libusb_context *ctx;
+} dfu_usb_dev_st_t;
+
+dfu_usb_error_t dfu_usb_library_init(void);
 void dfu_usb_library_deinit(void);
 
 #endif /* __DFU_USB_H__ */
